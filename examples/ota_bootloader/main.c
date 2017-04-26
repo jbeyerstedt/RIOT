e8da73bfd238e058027ea3345f3b683cff0c967a
Jannik Beyerstedt <code@jannikbeyerstedt.de>
2017-04-26 17:17:47 +0200

    f examples/ota_bootloader: shorten some strings

--- examples/ota_bootloader/main.c
+++ examples/ota_bootloader/main.c
@@ -41,11 +41,6 @@ static inline void iwdg_reload(void)
     IWDG->KR = 0xAAAA;
 }
 
-/* checks, if a reset was triggered because of the IWDG */
-static inline uint8_t get_iwdg_reset_flag(void) {
-    return RCC->CSR & RCC_CSR_WDGRSTF;
-}
-
 void iwdg_start_and_configure(void)
 {
     /* configure watchdog */
@@ -58,7 +53,7 @@ void iwdg_start_and_configure(void)
 
 void safe_state(void)
 {
-    printf("no bootable image found, go to safe state");
+    printf("no bootable image found, safe state");
     while (1) {
         iwdg_reload();
         xtimer_usleep(250000);
@@ -69,7 +64,7 @@ void boot_firmware_slot(uint8_t slot)
 {
     uint32_t address;
 
-    printf("booting image on slot %d ...\n", slot);
+    printf("booting FW slot %d ...\n", slot);
     address = ota_slots_get_slot_address(slot);
     ota_slots_jump_to_image(address);
 }
@@ -86,23 +81,22 @@ int main(void)
     iwdg_start_and_configure();
 
     /*** start of bootloader behaviour ***/
-    (void) puts("welcome to RIOT bootloader\n");
+    (void) puts("welcome to OTA bootloader\n");
 
     ota_slots_print_available_slots();
 
     /** first assume, that the newest slot is good **/
     boot_slot = ota_slots_find_newest_int_image();
     if (0 == boot_slot) {
-        (void) puts("no bootable slot found\n");
         safe_state();
     }
 
     /** check, if both slots are populated **/
     if (boot_slot == ota_slots_find_oldest_int_image()) {   /* just one slot */
-        printf("only one slot is populated, getting watchdog reset flag\n");
+        printf("one slot populated, getting watchdog reset flag\n");
         if (0 == reset_flag_iwdg) {             /* watchdog ok, normal behaviour */
             /* check signature before booting */
-            printf("reset status normal, validating signature of slot %i\n", boot_slot);
+            printf("reset flag normal, validating signature of slot %i\n", boot_slot);
             if (ota_slots_validate_int_slot(boot_slot) == 0) {
                 boot_firmware_slot(boot_slot);
             }
@@ -112,15 +106,15 @@ int main(void)
             }
         }
         else {                                  /* watchdog not ok, secondary behaviour */
-            printf("reset because of expired watchdog\n");
+            printf("reset reason was watchdog\n");
             safe_state();
         }
     }
     else {                                                  /* both slots are populetd */
-        printf("both slots are populated, getting watchdog reset flag\n");
+        printf("both slots populated, getting watchdog reset flag\n");
         if (0 == reset_flag_iwdg) {             /* watchdog ok, try normal behaviour */
             /* check signature of newest slot */
-            printf("reset status normal, validating signature of slot %i\n", boot_slot);
+            printf("reset flag normal, validating signature of slot %i\n", boot_slot);
             if (ota_slots_validate_int_slot(boot_slot) == 0) {
                 boot_firmware_slot(boot_slot);
             }
@@ -128,7 +122,7 @@ int main(void)
                 printf("slot %u inconsistent\n", boot_slot);
 
                 /* compare fw versions of the update file and the newest slot */
-                printf("checking, if the cause was an incomplete installation\n");
+                DEBUG("checking, if reason was an incomplete installation\n");
                 OTA_File_header_t *file_header = (OTA_File_header_t *)(OTA_FILE_SLOT);
                 /* first check if a file is available */
                 if ((file_header->magic_h == (uint32_t)OTA_FW_FILE_MAGIC_H) && (file_header->magic_l == (uint32_t)OTA_FW_FILE_MAGIC_L)) {
@@ -139,11 +133,11 @@ int main(void)
                     uint16_t slot_fw_vers = slot_metadata.fw_vers;
                     if (file_fw_vers <= slot_fw_vers) {
                         /* delete newest FW slot */
-                        printf("-> incomplete installation detected, erasing newest FW slot\n");
+                        printf("> incomplete installation detected, erasing slot\n");
                         flashsector_write(get_slot_page(boot_slot), NULL, 0);
                     }
                     else {
-                        printf("-> the cause must be a bitflip somewhere, or manipulation\n");
+                        DEBUG("> the cause must be a bitflip somewhere\n");
                     }
                 }
 
@@ -151,10 +145,10 @@ int main(void)
             }
         }
         else {                                  /* watchdog not ok, secondary behaviour */
-            printf("reset because of expired watchdog, installed FW is not executable\n");
+            printf("reset reason was watchdog, installed FW is not executable\n");
 
             /* compare fw versions of the update file and the newest slot */
-            printf("checking, if the cause was a freshly installed FW\n");
+            DEBUG("checking, if reason was a freshly installed FW\n");
             OTA_File_header_t *file_header = (OTA_File_header_t *)(OTA_FILE_SLOT);
             /* first check if a file is available */
             if ((file_header->magic_h == (uint32_t)OTA_FW_FILE_MAGIC_H) && (file_header->magic_l == (uint32_t)OTA_FW_FILE_MAGIC_L)) {
@@ -165,11 +159,11 @@ int main(void)
                 uint16_t slot_fw_vers = slot_metadata.fw_vers;
                 if (file_fw_vers <= slot_fw_vers) {
                     /* delete the malicious update file */
-                    printf("-> update file matches installed FW, erasing update file\n");
+                    printf("> update file matches installed FW, erasing update file\n");
                     flashsector_write(OTA_FILE_SLOT_START_SECTOR, NULL, 0);
                 }
                 else {
-                    printf("-> there is already a newer update file being downloaded\n");
+                    DEBUG("> reason unknown\n");
                 }
             }
 
@@ -177,7 +171,7 @@ int main(void)
         }
 
         /* set boot_slot to the old slot */
-        printf("newest FW slot is not executable, trying old FW\n");
+        DEBUG("newest FW slot is not executable, trying old FW\n");
         boot_slot = ota_slots_find_oldest_int_image();
 
         /* check signature before booting */
